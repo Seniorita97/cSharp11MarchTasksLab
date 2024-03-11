e733e8fd98b887430a75e9405b62fe468e5a73c0
Aytac <111279845+Seniorita97@users.noreply.github.com>
2024-03-11 09:55:58 +0400

    labtask finished

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,59 @@
+﻿//--------------------------------------------------------------------------------------------
+// tekrarlanmayanlari capa ver
+
+//int[] arr = { 2, 7, 3, 14, 8, 2, 3, 7 };
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    bool tekrarVar = false;
+
+//    for (int j = 0; j < arr.Length; j++)
+//    {
+//        if (i != j && arr[i] == arr[j])
+//        {
+//            tekrarVar = true;
+//            break;
+//        }
+//    }
+
+//    if (!tekrarVar)
+//    {
+//        Console.Write(arr[i] + " ");
+//    }
+//}
+
+
+//---------------------------------------------------------------------------------
+// arrayda her elementden nece denedir?
+
+//int[] arr = { 2, 5, 9, 2};
+//int[] arrcopy = new int[arr.Length];
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    arrcopy[i] = arr[i];
+//}
+//foreach (int item in arrcopy)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//-----------------------------------------------------------------------------------------------
+
+
+int[] arr = { 2, 5, 9, 2, 2, 11, 5 };
+
+foreach (int element in arr)
+{
+    int count = 0;
+
+    foreach (int innerElement in arr)
+    {
+        if (element == innerElement)
+        {
+            count++;
+        }
+    }
+
+    Console.WriteLine($"{element}: {count} dəfə");
+}
